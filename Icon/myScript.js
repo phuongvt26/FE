da39f12f88f4bab8a41911facc447a76d2e9aecb
phuongvt26 <phuonngfighting1998@gmail.com>
2023-08-21 23:03:14 +0700

    First commit

---
+++ Icon/myScript.js
@@ -0,0 +1,14 @@
+// function myFunction (){
+//     document.getElementById("demo").innerHTML="Sorrry";
+// }
+
+
+// <script>
+// document.getElementById("add").innerHTML = 5 + 6;
+// </script>
+
+// <script>
+// document.write = 5 + 6;
+// </script>
+var x = 1
+
